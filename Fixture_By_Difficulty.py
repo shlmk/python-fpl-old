87e9e3d283f001febfbb93ad721a9ed3e747bc48
shlmk <kingofsherlockspades@gmail.com>
2016-09-27 22:06:45 -0400

    Create df of diffculty fixtures

--- Fixture_By_Difficulty.py
+++ Fixture_By_Difficulty.py
@@ -11,6 +11,23 @@ import numpy as np
 import pandas as pd
 import ast
 
+def extract_ratings(fixtures, team_num):
+    print(len(fixtures))
+    diffculty = []
+    count = 0
+    for fix in fixtures: 
+        if(fix['team_h'] == team_num):
+            diffculty.append(fix['team_h_difficulty'])
+            print("YAS")
+            count += 1
+            print(count)
+        elif(fix['team_a'] == team_num):
+            diffculty.append(fix['team_a_difficulty'])
+            print("YAS2")
+            count += 1
+            print(count)
+    return diffculty
+    
 r = requests.get('https://fantasy.premierleague.com/drf/fixtures/')
                      
 soup = BeautifulSoup(r.text)
@@ -35,12 +52,14 @@ for idx, fix in enumerate(fixture_list):
     fix = fix.replace("null", '[]')
     fixture_list_corr.append(ast.literal_eval(fix))
     
-def extract_ratings(fixtures, team_num):
-    diffculty = []
-    for fix in fixtures: 
-        if(fix['team_h'] == team_num):
-            diffculty.append(fix['team_h_difficulty'])
-        elif(fix['team_a'] == team_num):
-            diffculty.append(fix['team_a_difficulty'])
-    return diffculty
+diff_by_team = {}    
+for team_num in range(1, 21):
+    diff = extract_ratings(fixture_list_corr,team_num)
+    diff_by_team[team_num] = np.array(diff)
+    
+all_schedules_df = pd.DataFrame.from_dict(diff_by_team, orient='index')
+all_schedules_df.columns = ['Week ' + str(i) for i in range(1, 39)]
+
+
+
     
