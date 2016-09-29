e5117e4ed342597c1c22333c01a22427923dc616
shlmk <kingofsherlockspades@gmail.com>
2016-09-29 19:04:23 -0400

    Begin reading week data

--- Color-Spreadsheet.py
+++ Color-Spreadsheet.py
@@ -5,3 +5,33 @@ Created on Thu Sep 29 18:05:04 2016
 @author: MMS
 """
 
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+from openpyxl import load_workbook
+
+#TODO: Make better placement of this
+teams = ['Arsenal','Bournemouth','Burnley',
+         'Chelsea', 'Crystal Palace', 'Everton',
+         'Hull','Leicester','Liverpool','Man City',
+         'Man United','Middlesbrough','Southampton',
+         'Spurs','Stoke','Sunderland','Swansea',
+         'Watford','West Brom','West Ham']
+         
+def process_row(row):
+    print("DOES NOTHING!")
+
+
+r = requests.get('http://www.livefootball.co.uk/premier-league/2016-2017/regular-season/gameweek-01/')
+soup = BeautifulSoup(r.text)
+
+rows = soup.find_all('tr')
+for row in rows:
+    if row.has_attr('data-match-id'):
+      print("yikes!")
+      for game in row.childGenerator():
+          print(game)
+          
