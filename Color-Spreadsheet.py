cd2c7cbeb5f8ab25d1246d39934ed4f1e8aa80ae
shlmk <kingofsherlockspades@gmail.com>
2016-09-29 22:29:27 -0400

    Read from FPL site. Convert result effectively

--- Color-Spreadsheet.py
+++ Color-Spreadsheet.py
@@ -12,6 +12,8 @@ import pandas as pd
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 from openpyxl import load_workbook
+import ast
+from functools import reduce
 
 #TODO: Make better placement of this
 teams = ['Arsenal','Bournemouth','Burnley',
@@ -20,34 +22,35 @@ teams = ['Arsenal','Bournemouth','Burnley',
          'Man United','Middlesbrough','Southampton',
          'Spurs','Stoke','Sunderland','Swansea',
          'Watford','West Brom','West Ham']
-         
+       
+def process_week(week_html):
+    week = week_html.text
+    
+    #From: https://stackoverflow.com/questions/6116978/python-replace-multiple-strings
+    repls = ('true','True'), ('false', 'False'), ('null', '[]')
+    week = reduce(lambda a, kv: a.replace(*kv), repls, week)
+    week = ast.literal_eval(week)
+            
 #NOTE: weeks must be a list format
 def record_data(weeks):
+    url = 'https://fantasy.premierleague.com/drf/fixtures/?event='
+
     if type(weeks) is not list:
         raise TypeError('Weeks must be of type list')
     else:
-        for week in weeks:
-            if type(week) is not int:
+        for week_num in weeks:
+            if type(week_num) is not int:
                 raise TypeError('Week must be an int value')
-            elif week < 1 or week > 38:
+            elif week_num < 1 or week_num > 38:
                 raise IndexError('Week must be between 1 and 38')
-            else:
-                process_week(week)
-                
-def process_week(week):
-    
+            else: 
+                url += str(week_num)
         
-def process_row(row):
-    print("DOES NOTHING!")
+            print(url)
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'lxml')
+            
+            process_week(soup)
 
 
-r = requests.get('http://www.livefootball.co.uk/premier-league/2016-2017/regular-season/gameweek-01/')
-soup = BeautifulSoup(r.text)
-
-rows = soup.find_all('tr')
-for row in rows:
-    if row.has_attr('data-match-id'):
-      print("yikes!")
-      for game in row.childGenerator():
-          print(game)
           
